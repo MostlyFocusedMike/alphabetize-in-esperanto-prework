00605131e45335dfa6f94836ef91461321c02c3a
Mike C <mikecronin92@gmail.com>
2018-03-30 05:23:10 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,9 +4,10 @@ require 'pry'
 def alphabetize(arr)
   # code here
   esp_alpha = 'abcĉdefgĝhĥijĵklmnoprsŝtuŭvz'
-  arr.sort do |a,b|
+  thing = arr.sort do |a,b|
     esp_alpha.index(a) <=> esp_alpha.index(b)    
   end
+  puts thing.inspect
 end
 
 arr = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
