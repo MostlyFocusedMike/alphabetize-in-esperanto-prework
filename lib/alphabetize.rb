61069a7109cfaa0d351587f2d7af142d71c2111c
Mike C <mikecronin92@gmail.com>
2018-03-30 05:25:51 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -5,7 +5,7 @@ def alphabetize(arr)
   # code here
   esp_alpha = 'abcĉdefgĝhĥijĵklmnoprsŝtuŭvz'
   thing = arr.sort do |a,b|
-    a,b = esp_alpha.index(a),esp_alpha.index(b) 
+    a,b = esp_alpha.index(a[0]),esp_alpha.index(b[0]) 
     a <=> b    
   end
   puts thing.inspect
