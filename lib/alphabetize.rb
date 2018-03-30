8cebce311f4e31b95e4b383da59b4b9ee42e60b9
Mike C <mikecronin92@gmail.com>
2018-03-30 05:03:27 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,12 @@
+binding.pry
+
 def alphabetize(arr)
   # code here
   Esp_alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |phrase|
-    
+    binding.pry
   end
 end
+
+phrase = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
+require 'pry'
